f29614f6e93096ef3b0e8ec9aba94bc71079f6b8
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2020-12-22 15:19:54 -0200

    reestruturação

--- src/pages/profile/index.js
+++
@@ -1,9 +0,0 @@
-
-export const Profile = () => {
-    const profileCreated = document.createElement('div');
-    profileCreated.innerHTML = `
-     <h1>Profile teste</h1>
-    
-    `
-    return profileCreated
-  };
