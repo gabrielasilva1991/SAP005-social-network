8c3e9a9b0371b44a809c158022da12605a3c7f63
Eliane <eliane.gomes.cardozo@gmail.com>
2021-01-11 01:16:24 -0300

    (src)Arquivos com atualizações e estudos para edição dos posts

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,5 +1,4 @@
-import { logOut, showPosts } from '../../services/index.js';
-import { creatPost, loadingPost } from '../../services/index.js'
+import { logOut, creatPost, loadingPost, deletePost } from '../../services/index.js'
 
 export const Posts = () => {
   const rootElement = document.createElement('div');
@@ -12,37 +11,172 @@ export const Posts = () => {
       <textarea id="new-post" rows="5" cols="50" placeholder="Escreva sua publicação"></textarea>
       <button type="submit" id="submit-post">Publicar</button>
     </form>
-
-    <div id="post-creat" ></div>
+    <div id="post-creat"></div>
   `;
 
   rootElement.querySelector("#logout").addEventListener("click", (e) => {
     e.preventDefault();
     return logOut();
   });
-  
+
+  const loadPost = () => {
+    loadingPost().then(results => {
+      let cardPost = document.querySelector("#post-creat")
+      cardPost.innerHTML= "";
+      
+      const div = document.createElement("div")
+      div.classList.add("post-creat")
+      
+      results.forEach(doc => {
+        showPosts({
+          //date: doc.data().date.toDate().toLocaleString('pt-BR'),
+          postId: doc.id,
+          userName: doc.data().userName,
+          userEmail: doc.data().userEmail,
+          text: doc.data().text,
+          likes: doc.data().likes,
+          // comments: doc.data().comments,
+          date: doc.data().date,
+          //time: data.getTime(),
+          //dataString: `${data.toLocaleDateString()} ${data.getHours()}:${data.getMinutes()}`,
+        });
+      });
+
+      //cardPost.appendChild(div)
+    });
+  };
+
+  rootElement.querySelector("#new-post").onload = loadPost();
+
   rootElement.querySelector("#submit-post").addEventListener("click", (e) => {
     e.preventDefault();
     const postInitial = rootElement.querySelector("#new-post").value;
-    creatPost(postInitial)  
-    loadingPost().then(results => {
-      document.querySelector("#post-creat").innerHTML= "";
-      console.log(results)
-      results.docs.map(doc => {
-        showPosts({      
-          id: doc.id,
-          userId: doc.data().userId,
-          text: doc.data().text,
-          likes: doc.data().likes,
-          comments: doc.data().comments,
-        });
-      });   
-    });  
-    console.log(loadingPost) 
+    creatPost(postInitial)
+    document.querySelector("#post-creat").innerHTML= "Carregando..."
+    loadPost()
   });
+    
   return rootElement;
 };
 
+const showPosts = (posts) => {
+
+  const postCreat = document.querySelector("#post-creat")
+  
+
+  const postsTemplates = `
+    <div id="${posts.id}">
+      <p>${posts.userName}</p>
+      <p>${posts.date}</p>
+      <p>${posts.text}</p>
+      <p>${posts.likes}</p>
+      
+
+      <button class="likes" id="likes">Curtir</button>
+      <button class="edit" id="edit">Editar</button>
+      <button class="delete" id="delete" div-id=${posts.id}>Deletar</button>
+    </div>
+  `
+  postCreat.innerHTML += postsTemplates; 
+
+  postCreat.querySelectorAll("#delete").forEach((e)=> {
+    e.addEventListener("click", (e) => {
+      e.target.parentNode.querySelector("#delete"); 
+      //e.target.parentNode.querySelector("#delete") 
+      //parent aplica para todos os delete da pagina
+      deletePost(posts.postId)
+      loadingPost()
+    });
+  })
+};
+
+// postCreat.querySelector("#edit").forEach((e) => {
+//   e.addEventListener("click", (e) => {
+//     e.target.dataset.id
+
+// })
+
+// const btnLike = document.querySelector("#likes");
+  // btnLike.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   likePost(posts)
+  // });
+ 
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+// feedArea.addEventListener('click', (event) => {
+//   const closestEditar = event.target.closest(btnEditar);
+//   if (closestEditar && feedArea.contains(closestEditar)) {
+//     const closestTextarea = closestEditar.parentNode.querySelector('.editar-post');
+//     closestTextarea.style.display = 'block';
+//     const closestBtnSalvarEdicao = closestEditar.parentNode.querySelector('.btn-salvar-editado');
+//     closestBtnSalvarEdicao.style.display = 'block';
+
+//     closestBtnSalvarEdicao.addEventListener('click', () => {
+//       closestTextarea.style.display = 'none';
+//       closestBtnSalvarEdicao.style.display = 'none';
+//       const closestPost = closestEditar.parentNode.querySelector('.texto-post');
+//       const postFinal = closestTextarea.value;
+//       closestPost.innerHTML = postFinal;
+//       const closestId = closestEditar.parentNode.querySelector('.id-escondido').innerText;
+
+
+
+
+// const editPost = (e) => {
+//   const postId = e.target.dataset.id;
+//   e.target.disabled = true;
+//   const postContent = document
+//     .getElementById(postId)
+//     .querySelector('.post-content');
+//   postContent.innerHTML = ` 
+//   ${window.textarea.component({
+//     class: 'edit-textarea',
+//     text: postContent.innerHTML
+//   })}
+//   ${window.button.component({
+//     dataId: postId,
+//     id: 'edit-button',
+//     class: 'oval-button ',
+//     title: 'Salvar',
+//     call: window.home.saveEdit
+//   })} `;
+// }
+
+// function editPost(event) {
+//   const id = event.target.dataset.id;
+//   document.getElementById('post_' + id).contentEditable = true;
+//   document.getElementById('post_' + id).style.border = '1px solid black';
+//   document.querySelector('#edit-' + id).innerHTML = '✔️';
+//   document.querySelector('#edit-' + id).addEventListener('click', window.feed.saveEdit);
+// }
+
+// function saveEdit() {
+//   const id = event.target.dataset.id;
+//   document.getElementById('post_' + id).contentEditable = false;
+//   document.getElementById('post_' + id).style.border = '';
+//   document.querySelector('#edit-' + id).innerHTML = '✏️';
+//   const text = document.querySelector('#post_' + id).textContent;
+//   const timestamp = new Date().toLocaleDateString('pt-BR') + ' - ' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
+//   firebase.firestore().collection('posts').doc(id).update({ text, timestamp });
+//   document.querySelector('#edit-' + id).removeEventListener('click', window.feed.saveEdit);
+
+  // const likesStored = rootElement.querySelector("#like").value;
+
+ 
 
 // const showPosts = () => {
 //   const rootElement = document.createElement('div');
--- src/services/index.js
+++ src/services/index.js
@@ -7,7 +7,8 @@ export const loginWithGoogle = () => {
   .then(() => {
     const user = firebase.auth().currentUser
     checkLogin(user)
-    alert('Login realizado com sucesso')
+    // alert('Login realizado com sucesso')
+    alert(`Olá, ${firebase.auth().currentUser.displayName}`)
   })
   .catch(() => {
     alert('Você não conectou com o Google, tente novamente')
@@ -20,6 +21,7 @@ export const registerUser = (email, password) => {
     const user = firebase.auth().currentUser
     checkLogin(user)
     alert('Conta criada com sucesso')
+    alert(`Olá, ${document.querySelector("#name").value}`)
   })
   .catch(() => {
     alert('Falha ao realizar o cadastro')
@@ -31,7 +33,8 @@ export const signIn = (email, password) => {
   .then(() => {
     const user = firebase.auth().currentUser
     checkLogin(user)
-    alert('Login realizado com sucesso')
+     alert('Login realizado com sucesso')
+     alert(`Olá, ${firebase.auth().currentUser.displayName}`) //NÃO ESTÁ APARECENDO O NOME DO USUÁRIO: NULL
   })
   .catch(() => {
     alert('Email e/ou senha incorretos')
@@ -41,8 +44,9 @@ export const signIn = (email, password) => {
 export const logOut = () => {
   firebase.auth().signOut()
   .then(() => {
-    history.pushState(null, null, window.location.pathname)})
+    history.pushState(null, null, window.location.pathname)
     alert('Desconectado')
+  });
 };
 
 export const checkLogin = () => {
@@ -54,30 +58,46 @@ export const checkLogin = () => {
     }
   });
 };
-
+ 
 //PÁGINA DE POSTS
-export const creatPost = (postInicial) => {
+export const creatPost = (postInicial, likesStored) => {
   firebase.firestore().collection("posts").add({
-    userId: `${firebase.auth().currentUser.email}`, //(identifica o usuário que está logado)
+    userName: `${firebase.auth().currentUser.displayName}`,
+    userEmail: `${firebase.auth().currentUser.email}`, //(identifica o usuário que está logado)
     text: `${postInicial}`, // (uau, aqui vai o texto, minha nossa!!! innerText/innerHtml)
-    likes: [], // (array vazio de likes)
-    comments: [], // (array vazio de comentários)
+    likes: `${likesStored}`, // (array vazio de likes)
+    // comments: [], // (array vazio de comentários)
+    //date: new date(),
+    //date: date.toLocaleString(),
+    //time: date.getTime(),
   });
 };
 
-export const loadingPost = () => {
+export const loadingPost = (postList) => {
   return firebase.firestore().collection("posts").get()
+  //.orderBy("date", "desc")
 };
 
-export const showPosts = (posts) => {
-  const postsTemplates = `
-    <div>
-      <p>${posts.text}</p>
-    </div>
-  `
-  const postCreat = document.querySelector("#post-creat")
-  postCreat.innerHTML += postsTemplates; 
+// export const likePost = (postId) => {
+//   return firebase.firestore().collection("posts").doc(postId).update({
+//     likes: firebase.firestore.FieldValue.increment(1) 
+//     //aumenta valor numérico, úteis para implementar contadores
+//   });
+// };
+
+// export const editPost = (postId) => {
+//   return firebase.firestore().collection("posts").doc(postId).update({
+//     text: textArea.value
+//   });
+// };
+
+export const deletePost = (postId) => {
+  return firebase.firestore().collection("posts").doc(postId).delete()
 };
+
+//export const commentsPosts = (postId) => {
+  //return firebase.firestore().collection("posts").doc(postId) 
+//};
 
 
 //PÁGINA DE POSTS ISA
