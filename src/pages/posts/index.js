b8dc390459a86df0f3a6c82748259fb5bcf55946
Eliane <eliane.gomes.cardozo@gmail.com>
2021-01-12 17:31:54 -0300

    (src):Alterado arquivos com refatoração

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,21 +1,21 @@
-import { logOut, creatPost, getPost, likePost, deletePost } from '../../services/index.js'
+import { logOut, creatPost, getPost, likePost, deletePost } from '../../services/index.js';
 
 export const Posts = () => {
-  const rootElement = document.createElement('div');
+  const rootElement = document.createElement("div");
   rootElement.innerHTML = `
     <header>
-      <button id='logout'>Sair</button>
+      <button id="logout">Sair</button>
     </header>
     <h1 class="user-name" id="user-name">Postagens</h1>
     <form action="" class="form-post" id="form-post">
       <textarea id="new-post" rows="5" cols="50" placeholder="Escreva sua publicação"></textarea>
       <button type="submit" id="submit-post">Publicar</button>
     </form>
-    <div id="post-creat"></div>
+    <div id="container-post"></div>
   `;
 
   rootElement.querySelector("#logout").addEventListener("click", (e) => {
-    e.preventDefault();loading
+    e.preventDefault();
     return logOut();
   });
 
@@ -28,153 +28,44 @@ export const Posts = () => {
     creatPost(postCreat)
     getPost(showPosts)
     newPost.value = ""
-    rootElement.querySelector("#post-creat").innerHTML= "";
+    rootElement.querySelector("#container-post").innerHTML= "";
   });
-    
   return rootElement;
 };
 
-const showPosts = (posts) => {
-  console.log(posts)
-
-  const postCreat = document.querySelector("#post-creat")
+const showPosts = (posts) => { 
+  const postCreat = document.querySelector("#container-post")
   
+  postCreat.innerHTML += `
+    <div class="post-individual" id="${posts.postId}">
 
-  const postsTemplates = `
-    <div class="pos-individual" id="${posts.postId}">
-      <p>${posts.userName}</p>
-      <p>${posts.date}</p>
-      <p>${posts.text}</p>
-      <p>${posts.likes}</p>
+      <div class="post-text">
+        <p id="show-text">${posts.text}</p>
+        <p id="show-name">${posts.userName} </p>
+        <p id="show-date">${posts.date} </p> 
+        <p id="show-like">${posts.likes}</p>
+      </div>
+
+      <div class="post-edit"></div>
       
+      <div class="button">
+        <button class="like" data-like="${posts.postId}">Curtir</button>
+        <button class="edit" data-edit="${posts.postId}">Editar</button>
+        <button class="delete" data-id="${posts.postId}">Deletar</button>
+      </div>
 
-      <button class="like" data-like="${posts.postId}">Curtir</button>
-      <button class="edit" data-edit="${posts.postId}">Editar</button>
-      <button class="delete" data-id="${posts.postId}">Deletar</button>
-    </div>
-  `
-  postCreat.innerHTML += postsTemplates; 
+    </div>  
+  `;
 
-  // postCreat.querySelectorAll(".like").forEach((e)=> {
-  //   e.addEventListener("click", (e) => {
-  //     e.target.parentNode.classList.add("likes"); 
-  //     likePost(posts.postId).then(() => {
-  //       console.log("Curtiu")
-  //     })
-  //     .catch(() => {
-  //       alert("Tente curtir novamente")
-  //     });
-  //   });
-  // })
-
-
-  const btnLike = postCreat.querySelectorAll(".like");
-
-  btnLike.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      executeLike(e)
+  postCreat.querySelectorAll(".like").forEach((e)=> {
+    e.addEventListener("click", (e) => { 
+      likePost(e.target.dataset.like)
     });
-  }) 
-
-  const executeLike = (e) => {
-    likePost()
-    .then((retorno) => {
-      console.log(retorno)
-      e.target.classList.add("likes")
-    })
-    .catch(() => {
-      alert("Tente curtir novamente")
-    });
-  }
-
+  });
 
   postCreat.querySelectorAll(".delete").forEach((e)=> {
     e.addEventListener("click", (e) => {
-      e.target.parentNode.querySelector(".delete") // e.target pega o elemento que cliquei  
-      //parent aplica para todos os delete da pagina // sempre volta a uma casa anteriror(div)
-      deletePost(posts.postId)
+      deletePost(e.target.dataset.id) 
     });
-  })
+  });
 };
-
-  // postCreat.querySelectorAll(".delete").forEach((e)=> {
-  //   e.addEventListener("click", (e) => {
-  //     e.target.parentNode.querySelector(".delete") 
-  //     //parent aplica para todos os delete da pagina // sempre volta a uma casa anteriror(div)
-  //     deletePost(posts.postId)
-  //   });
-  // }) 
-
-
-  // const executedelete = (e) => {
-  //   deletePost().then(() => {
-  //     console.log("Deletou")
-  //   })
-  //   .catch(() => {
-  //     alert("Tente deletar novamente")
-  //   });
-  // }
-
-
-
-
-
-
-  // postCreat.querySelectorAll(".like").forEach((e)=> {
-  //   e.addEventListener("click", (e) => {
-  //     e.target.parentNode.querySelector(".like"); 
-  //     likePost(posts.postId)
-  //     loadingPost()
-  //   });
-  // })
-
-  
-
-  // postCreat.querySelectorAll(".edit").forEach((e) => {
-  //   e.addEventListener("click", (e) => {
-  //     e.target.dataset.id.querySelector(".edit");
-  //     editPost(posts.postId)
-  //     loadingPost()
-  //   });
-  // })
-
- 
-  //MODELO EX ALUNA
-
-  // feedArea.addEventListener('click', (event) => {
-  //   const closestEditar = event.target.closest(btnEditar);
-  //   if (closestEditar && feedArea.contains(closestEditar)) {
-  //     const closestTextarea = closestEditar.parentNode.querySelector('.editar-post');
-  //     closestTextarea.style.display = 'block';
-  //     const closestBtnSalvarEdicao = closestEditar.parentNode.querySelector('.btn-salvar-editado');
-  //     closestBtnSalvarEdicao.style.display = 'block';
-
-  //     closestBtnSalvarEdicao.addEventListener('click', () => {
-  //       closestTextarea.style.display = 'none';
-  //       closestBtnSalvarEdicao.style.display = 'none';
-  //       const closestPost = closestEditar.parentNode.querySelector('.texto-post');
-  //       const postFinal = closestTextarea.value;
-  //       closestPost.innerHTML = postFinal;
-  //       const closestId = closestEditar.parentNode.querySelector('.id-escondido').innerText;
-
-
-
-
-
-
-  
-
-  // const showPosts = () => {
-  //   const rootElement = document.createElement('div');
-  //   const posts = getPosts()
-  //   let element = ''
-
-  //   posts.map(post => {
-  //     element += `
-  //       <p>${post.message} </p>
-  //     `;
-  //   });
-
-  //   rootElement.innerHTML = element
-  //   showPosts() 
-  // };
--- src/services/index.js
+++ src/services/index.js
@@ -7,24 +7,23 @@ export const loginWithGoogle = () => {
   .then(() => {
     const user = firebase.auth().currentUser
     checkLogin(user)
-    // alert('Login realizado com sucesso')
     alert(`Olá, ${firebase.auth().currentUser.displayName}`)
   })
   .catch(() => {
-    alert('Você não conectou com o Google, tente novamente')
+    alert("Você não conectou com o Google, tente novamente")
   });
 };
 
 export const registerUser = (email, password) => {
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(() => {
-    const user = firebase.auth().currentUser
+    const user = firebase.auth().currentUser.displayName
     checkLogin(user)
-    alert('Conta criada com sucesso')
+    alert("Conta criada com sucesso")
     alert(`Olá, ${document.querySelector("#name").value}`)
   })
   .catch(() => {
-    alert('Falha ao realizar o cadastro')
+    alert("Falha ao realizar o cadastro")
   });
 };
 
@@ -33,43 +32,36 @@ export const signIn = (email, password) => {
   .then(() => {
     const user = firebase.auth().currentUser
     checkLogin(user) 
-     alert('Login realizado com sucesso')
+     alert("Login realizado com sucesso")
      //alert(`Olá, ${firebase.auth().currentUser.displayName}`) //NÃO ESTÁ APARECENDO O NOME DO USUÁRIO: NULL
   })
   .catch(() => {
-    alert('Email e/ou senha incorretos')
+    alert("Email e/ou senha incorretos")
   });
 };
 
 export const logOut = () => {
   firebase.auth().signOut()
-  .then(() => {
-    history.pushState(null, null, window.location.pathname)
-    alert('Desconectado')
-  });
 };
 
-export const checkLogin = () => {
+const checkLogin = () => {
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      onNavigate('/posts')
+      onNavigate("/posts")
     } else {
-      onNavigate('/')
+      onNavigate("/")
     }
   });
 };
  
 //PÁGINA DE POSTS
 export const creatPost = (postCreat) => {
-  // const x = firebase.auth().currentUser
-  // console.log(x)
   firebase.firestore().collection("posts").add({
     userName: firebase.auth().currentUser.displayName,
-    userEmail: firebase.auth().currentUser.email, //(identifica o usuário que está logado)
-    text: postCreat, // (uau, aqui vai o texto, minha nossa!!! innerText/innerHtml)
+    userEmail: firebase.auth().currentUser.email, 
+    text: postCreat, 
     likes: 0,
     date: new Date().toLocaleString(),
-    // comments: [], // (array vazio de comentários)
   });
 };
 
@@ -84,43 +76,14 @@ export const getPost = (showPosts) => {
         text: doc.data().text,
         likes: doc.data().likes,
         date: doc.data().date,
-        // comments: doc.data().comments,
       });
     });
   })
 };
 
-
-
-
-// export const likePost = (postId) => {
-//   return firebase.firestore().collection("likes").doc(postId).update({
-//     liked: firebase.firestore().FieldValue.increment(1) 
-//     //aumenta valor numérico, úteis para implementar contadores
-//   });
-// };
-
-// export const likePost = () => {
-//   return firebase.firestore().collection("likes").add({
-//     likes: [],
-//   })
-//   .then(() => {
-//     console.log("Deu certo")
-//     return Promise.resolve(true)
-//   })
-//   .catch((error) => {
-//     console.log("Deu ruim")
-//     return Promise.reject(error)
-//   })
-// }
-
 export const likePost = (postId) => {
-  return firebase.firestore().collection("posts").doc(postId).get()
-  .then((post) => {
-    const likes = (post.data().likes) + 1;
-    firebase.firestore().collection("posts").doc(postId).update({
-      likes,
-    });
+  return firebase.firestore().collection("posts").doc(postId).update({
+    likes: firebase.firestore.FieldValue.increment(1)
   });
 };
 
@@ -134,26 +97,3 @@ export const deletePost = (postId) => {
 //   });
 // };
 
-//export const commentsPosts = (postId) => {
-  //return firebase.firestore().collection("posts").doc(postId) 
-//};
-
-
-//PÁGINA DE POSTS ISA
-
-// const posts = [
-//   {message: 'Post 1'},
-//   {message: 'Post 2'},
-//   {message: 'Post 3'}
-// ]
-// export const getPosts = () => {
-//   //alterar para o get do firebase
-//   return posts
-// }
-
-// export const createPost = (post) => {
-//   //alterar parao add do firebase
-//   posts.push ({
-//     message: `${post} ${posts.length + 1}`
-//   })
-// }
